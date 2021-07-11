d890309680d0b1d5f927f13e13740feecbc71eed
HARIHARAN B P <bphariharan1301@gmail.com>
2021-07-11 21:53:50 +0530

    Search And Book page done

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from books.forms import SearchForm
 from django.shortcuts import get_object_or_404, render
-from .models import Book
+from .models import Book, Category
 from .choice import category_choices
 # Create your views here.
 
@@ -25,28 +25,32 @@ def book(request, book_id):
 
 
 def search(request):
-    books = Book.objects.all()
+    queryset_list = Book.objects.order_by('-book_date')
 
+    cats = Category.objects.all()
+
+    # Book Name
     if 'name' in request.GET:
         name = request.GET['name']
         if name:
-            books = books.filter(name__iexact=name)
+            queryset_list = queryset_list.filter(name__icontains=name)
 
+    # Author Name
     if 'author' in request.GET:
         author = request.GET['author']
         if author:
-            books = books.filter(author__iexact=author)
+            queryset_list = queryset_list.filter(author__iexact=author)
 
+    # Category
     if 'category' in request.GET:
         category = request.GET['category']
         if category:
-            books = books.filter(category__iexact=category)
-    print(books)
+            queryset_list = queryset_list.filter(category__iexact=category)
+
     context = {
-        'category_choices': category_choices,
-        'books': books,
-        'values': request.GET,
-        # 'list1': list1,
+        'books': queryset_list,
+        'cats': cats,
+        'values': request.GET
     }
 
     return render(request, 'books/search.html', context)
